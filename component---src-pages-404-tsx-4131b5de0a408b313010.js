e4a9bbe3b8a5415fe6402c001a634314174f97d9
이현수 <hs@ihyeonsuui-MacBookPro.local>
2023-10-18 03:07:47 +0900

    Updates

---
+++ component---src-pages-404-tsx-4131b5de0a408b313010.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[218],{2513:function(t,e,l){l.r(e);var a=l(7294),n=l(7959);e.default=t=>{var e;let{data:l,location:r}=t;const u=(null===(e=l.site.siteMetadata)||void 0===e?void 0:e.title)||"Title";return a.createElement("div",null,a.createElement("h1",null,u),a.createElement(n.Z,{title:"404: Not Found"}),a.createElement("h2",null,"404: Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-4131b5de0a408b313010.js.map
